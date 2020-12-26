81a3e0018c0c981eebb9cdc3c6f7b3963229345e
michellimacelebrar <michel@celebrar.co>
2020-12-26 16:36:53 -0300

    Created header on home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Pokemon from '../../Components/Pokemon';
 
-import { Container } from './styles';
+import { Container, Header } from './styles';
+import SearchIcon from '../../assets/images/Icons/SearchIcon.svg';
 
 function Home() {
 
@@ -14,21 +15,34 @@ function Home() {
         .then( pokedex => setPokeList(pokedex.results))
     }, []);
 
-  return (
-      <div className="container">
-        <Container>
-            {
-                pokelist.map( poke => {
-                    return (
-                        <Link to={`/pokemon/${poke.name}`} key={poke.id}>
-                            <Pokemon url={poke.url} key={poke.name} />
-                        </Link>   
-                    )
-                })
-            }
-        </Container>
-    </div>
-  );
+    return (
+        <div>
+            <Header>
+                <div className="container">
+                    <h1> Pokedex </h1>
+                    <input type="text" placeholder="Search"/>
+                    <figure>
+                        <img src={SearchIcon} alt=""/>
+                    </figure>
+                    <a href=""> See all types </a>
+                </div>
+            </Header>
+
+            <div className="container">
+                <Container>
+                    {
+                        pokelist.map( poke => {
+                            return (
+                                <Link to={`/pokemon/${poke.name}`} key={poke.id}>
+                                    <Pokemon url={poke.url} key={poke.name} />
+                                </Link>   
+                            )
+                        })
+                    }
+                </Container>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Bg from '../../assets/images/bgHome.svg';
 
 export const Container = styled.div `
     width: 100%;
@@ -10,5 +11,40 @@ export const Container = styled.div `
     @media (min-width: 600px){
         grid-template-columns: repeat(3, 1fr);
         grid-gap: 1rem;
+    }
+`;
+
+export const Header = styled.header`
+    width: 100%;
+    height: 18rem;
+    background-image: url(${Bg});
+    background-size: auto;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    
+    .container{
+        position: relative;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-end;
+        color: white;
+        padding-bottom: 2rem;
+
+        h1{
+            text-shadow: 0 0.1rem 0.3rem rgba(0, 0, 0, 0.5);
+        }
+
+        figure{
+            width: 2rem;
+            position: absolute;
+            right: 1rem;
+            top: 11.2rem;
+
+            img{
+                width: 100%;
+            }
+        }
     }
 `;
